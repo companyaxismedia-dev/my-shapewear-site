950bd8af324e38b35a02c2f1a45416ab000f9e33
companyaxismedia-dev <companyaxismedia@gmail.com>
2026-02-13 17:57:41 +0530

    Update LoginModal.jsx

--- frontend/src/app/authPage/LoginModal.jsx
+++ frontend/src/app/authPage/LoginModal.jsx
@@ -6,12 +6,8 @@ import axios from "axios";
 import { useAuth } from "@/context/AuthContext";
 import AuthModal from "./AuthModal";
 
-const API_BASE =
-  typeof window !== "undefined" &&
-  (window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1")
-    ? "http://localhost:5000"
-    : "https://my-shapewear-site.onrender.com";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+
 
 /* ================= REUSABLE INPUT ================= */
 export function AuthInput({
